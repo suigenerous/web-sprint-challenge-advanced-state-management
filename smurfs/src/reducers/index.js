5eb1d080c9cbe07c0b9996e4c3a6ecac8725b4ba
William Herman <williamajherman@gmail.com>
2020-09-11 11:18:55 -0400

    setup store

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+    smurfData: [],
+    errors: ''
+}
+
+export const smurfReducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
